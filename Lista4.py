ad5e460776d216a7d960c16fc3d6ceead0a4b52b
Arthur Nogueira <arthur.lima99@aluno.ce.gov.br>
2025-09-09 11:10:49 -0300

    ListaDeQuestões

---
+++ Lista4.py
@@ -0,0 +1,77 @@
+ # Primeira Questão
+
+# def saudacao(nome):
+#     return (f"Saudações, {nome}!")
+# print(saudacao("Maria"))
+
+# # Segunda Questão
+
+# def num(x):
+#     if x%2==0:
+#         return ("numero par")
+#     elif x%2==1:
+#         return ("numero impar")
+# print(num(3))
+
+# Terceira Questão
+
+# def num(x: int,y: int):
+#     if x>y:
+#         return (f"{x} é maior que {y}")
+#     elif x<y:
+#         return (f"{y} é maior que {x}")
+# print(num(1,-3))
+
+# Quarta Questão
+
+# num=int(input("Diga um numero: "))
+# for x in range(1,11):
+#     tabuada= num*x
+#     print(f"{num} x {x} = {tabuada}")
+
+# Quinta Questão
+
+# x=10
+# while x>=1:
+#     print(x)
+#     x-=1
+#     if x==0:
+#         print("Explosão")
+        
+# Sexta Quetão
+
+# def media(x,y,z):
+#     return (f"Sua média é: {(x+y+z) /3}")
+# print(media(10,10,10))
+
+# # Sétima Questão
+
+# def calcular_fatorial(numero):
+#     resultado = 1
+#     for i in range(1, numero + 1):
+#         resultado *= i
+#     return resultado
+# print(calcular_fatorial(6))
+
+# Oitava Questão
+
+# def vogal(palavra: str):
+#     numerodevogal=0
+#     for i in palavra:
+#         if i =="a" or i=="e" or i=="i" or i=="o" or i=="u":
+#             numerodevogal+=1
+#             return (f" Vogais totais={numerodevogal}")
+# palavra=str(input("Digite uma palavra"))
+# print(vogal(palavra))
+
+# Nona Questão
+
+import random
+numale= random.randint(1,20) 
+tenta=int(input("Me diga um numero aleatório de 1 a 20"))
+for x in 1
+if tenta>20:
+    print("Seu numero é maior que 20")
+    tenta=input("Diga outro numero, de 1 a 20")    
+   
+    
